b80523d05fbd0b339ace673c0664f177df486bf7
SemenZvehintsev <103433501+SemenZvehintsev@users.noreply.github.com>
2022-08-14 15:37:13 +0000

    Deploying to gh-pages from @ SemenZvehintsev/team-project-icecream-7@332a1ad8c2645de8fe8ef75602fb0576bac9a4ca 🚀

---
+++ index.38694450.js
@@ -0,0 +1,2 @@
+$(".multiple-items").slick({autoplay:!0,autoplaySpeed:2e3,dots:!0,infinite:!0,slidesToShow:1,slidesToScroll:1});
+//# sourceMappingURL=index.38694450.js.map
---
+++ index.e2cdb80d.js
@@ -0,0 +1,2 @@
+$(".multiple-items").slick({autoplay:!0,autoplaySpeed:2e3,dots:!0,infinite:!0,slidesToShow:1,slidesToScroll:1});
+//# sourceMappingURL=index.e2cdb80d.js.map
