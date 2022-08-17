95ea939f5e69fda3c094f950b4be59c61fa5e876
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-08-17 16:29:14 +0300

    added js close

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobMenu =  document.querySelector('.header__mobile-list');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +18,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  mobMenu.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
